a8bee5d01261a1ceaa13b12e7ba8a1fc5abddf7c
Lauren Ashley <la.zaubi@gmail.com>
2022-10-11 15:59:35 -0700

    not sure wth I changed...

--- timer1.js
+++ timer1.js
@@ -1,1 +1,18 @@
 
+const beep = (ms) => {
+  const isInvalid = typeof ms !== 'number' || ms < 0;
+  if (isInvalid) return;
+  setTimeout(() => {
+    process.stdout.write('\x07');
+  }, ms);
+};
+
+beep(300);
+beep(500);
+beep(900);
+beep(1000);
+beep(1500);
+beep(-100);
+beep();
+beep('3000');
+beep('-300');
