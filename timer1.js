8108ee3c39cfdd34527e963374533ccd181ea9e5
Lauren Ashley <la.zaubi@gmail.com>
2022-10-14 11:02:35 -0700

    adding changes to convert to using process.argv, not workinng yet

--- timer1.js
+++ timer1.js
@@ -1,18 +1,26 @@
+const args = process.argv.slice(2);
 
 const beep = (ms) => {
-  const isInvalid = typeof ms !== 'number' || ms < 0;
+  const notAnumber = typeof ms !== 'number' || isNaN(ms);
+  const isInvalid = notAnumber || ms < 0;
+  console.log(isInvalid);
   if (isInvalid) return;
   setTimeout(() => {
     process.stdout.write('\x07');
   }, ms);
 };
 
-beep(300);
-beep(500);
-beep(900);
-beep(1000);
-beep(1500);
-beep(-100);
-beep();
-beep('3000');
-beep('-300');
+for (const time of args) {
+  beep(time);
+  console.log(typeof time);
+}
+
+// beep(300);
+// beep(500);
+// beep(900);
+// beep(1000);
+// beep(1500);
+// beep(-100);
+// beep();
+// beep('3000');
+// beep('-300');
